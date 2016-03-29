5f136a4c440f709e1c1bc82cea47ef37d7d986d0
shibukk <hogemail009@gmail.com>
2016-03-29 10:09:43 +0900

    リリースshellをhomebrew用に修正

--- release.sh
+++ release.sh
@@ -4,9 +4,20 @@ XC_ARCH=${XC_ARCH:-386 amd64}
 XC_OS=${XC_OS:-linux darwin windows}
 
 rm -rf pkg/
+rm -rf pkg-tmp/
+
 gox \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
-    -output "pkg/{{.Dir}}_{{.OS}}_{{.Arch}}"
+    -output "pkg-tmp/{{.OS}}_{{.Arch}}/{{.Dir}}"
+
+mkdir pkg/
+for file in `\find ./pkg-tmp -type f`; do
+    FILE_NAME=${file##*/}
+    FILE_PATH=${file%/*}
+    `zip -jq ./pkg/${FILE_NAME%.*}_${FILE_PATH#*/*/}.zip ${file}`
+done
 
 ghr ${1:-v1.0.0} pkg/
+
+rm -rf pkg-tmp/
